745470358cf3d0530e46e1630da76a6d81f96a7b
Sameh Mohamed <126187468+samehmohamedka7raba@users.noreply.github.com>
2025-07-28 14:24:43 +0300

    Add project files.

---
+++ Advainced2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Advainced2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
